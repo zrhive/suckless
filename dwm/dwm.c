9f792cda74e52f354f03bf95f98c411e5feb29d5
zhyie <128297362+zhyie@users.noreply.github.com>
2026-04-25 19:39:39 +0800

    Adjust status padding

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -963,16 +963,16 @@ drawbar(Monitor *m)
 			if ((unsigned char)(*s) < ' ') {
 				ch = *s;
 				*s = '\0';
-				tw = TEXTW(text) - lrpad + 1;
+				tw = TEXTW(text) - lrpad;
 				drw_text(drw, m->ww - statusw - stw + x, 0, tw, bh, 0, text, 0);
 				x += tw;
 				*s = ch;
 				text = s + 1;
 			}
 		}
-		tw = TEXTW(text) - lrpad + 2;
-		// tw = TEXTW(text) - lrpad / 2 + 2;
-		drw_text(drw, m->ww - statusw - stw + x, 0, tw, bh, 0, text, 0);
+		// tw = TEXTW(text) - lrpad + 2;
+		tw = TEXTW(text) - lrpad / 2 + 2;
+		drw_text(drw, m->ww - statusw - stw + x, 0, tw, bh, lrpad / 2 - 2, text, 0);
 		tw = statusw;
 	}
 
