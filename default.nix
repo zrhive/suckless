3ee01b2193770d72ce2fb7f96a75436226088d29
zhyie <128297362+zhyie@users.noreply.github.com>
2026-04-26 20:33:15 +0800

    Move suckless packages to default nix file

--- default.nix
+++ default.nix
@@ -1,39 +1,57 @@
 {
-  config,
-  lib,
-  pkgs,
-  ...
-}:
-let
-  inherit (lib) mkEnableOption mkIf mkMerge;
-  cfg = config.suckless;
-in
-{
-  options.suckless = {
-    dwm = mkEnableOption "dwm";
-    slstatus = mkEnableOption "slstatus";
-    dmenu = mkEnableOption "dmenu";
-    st = mkEnableOption "st";
-  };
+  packages =
+    { pkgs }:
+    {
+      dwm = pkgs.dwm.overrideAttrs (old: {
+        src = ./dwm;
+        buildInputs = old.buildInputs ++ [ pkgs.libxcursor ];
+      });
+      st = pkgs.st.overrideAttrs (old: {
+        src = ./st;
+        buildInputs = old.buildInputs ++ [ pkgs.libxcursor ];
+      });
+      dmenu = pkgs.dmenu.overrideAttrs { src = ./dmenu; };
+      slstatus = pkgs.slstatus.overrideAttrs { src = ./slstatus; };
+    };
 
-  config = mkMerge [
-    (mkIf cfg.dwm {
-      services.xserver.windowManager.dwm = {
-        enable = true;
-        package = pkgs.dwm;
+  module =
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    let
+      inherit (lib) mkEnableOption mkIf mkMerge;
+      cfg = config.suckless;
+    in
+    {
+      options.suckless = {
+        dwm = mkEnableOption "dwm";
+        slstatus = mkEnableOption "slstatus";
+        dmenu = mkEnableOption "dmenu";
+        st = mkEnableOption "st";
       };
-    })
-    (mkIf cfg.dmenu {
-      environment.systemPackages = [ pkgs.dmenu ];
-    })
-    (mkIf cfg.st {
-      environment.systemPackages = [ pkgs.st ];
-    })
-    (mkIf cfg.slstatus {
-      environment.systemPackages = [ pkgs.slstatus ];
-      services.xserver.windowManager.dwm = {
-        extraSessionCommands = "${pkgs.slstatus}/bin/slstatus &";
-      };
-    })
-  ];
+
+      config = mkMerge [
+        (mkIf cfg.dwm {
+          services.xserver.windowManager.dwm = {
+            enable = true;
+            package = pkgs.dwm;
+          };
+        })
+        (mkIf cfg.dmenu {
+          environment.systemPackages = [ pkgs.dmenu ];
+        })
+        (mkIf cfg.st {
+          environment.systemPackages = [ pkgs.st ];
+        })
+        (mkIf cfg.slstatus {
+          environment.systemPackages = [ pkgs.slstatus ];
+          services.xserver.windowManager.dwm = {
+            extraSessionCommands = "${pkgs.slstatus}/bin/slstatus &";
+          };
+        })
+      ];
+    };
 }
--- flake.nix
+++ flake.nix
@@ -4,34 +4,21 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs =
-    { nixpkgs, ... }:
+    { self, nixpkgs }:
     let
+      suckless = import ./.;
+      lib = nixpkgs.lib;
       systems = [ "x86_64-linux" ];
       eachSystem = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
-
-      suckless-packages =
-        { pkgs }:
-        {
-          dwm = pkgs.dwm.overrideAttrs (old: {
-            src = ./dwm;
-            buildInputs = old.buildInputs ++ [ pkgs.libxcursor ];
-          });
-          st = pkgs.st.overrideAttrs (old: {
-            src = ./st;
-            buildInputs = old.buildInputs ++ [ pkgs.libxcursor ];
-          });
-          dmenu = pkgs.dmenu.overrideAttrs { src = ./dmenu; };
-          slstatus = pkgs.slstatus.overrideAttrs { src = ./slstatus; };
-        };
     in
     {
       nixosModules = {
-        default = import ./.;
+        default = suckless.module;
 
-        suckless =
-          { lib, ... }:
-          {
-            imports = [ ./. ];
+        suckless = (
+          self.nixosModules.default
+          // {
+            nixpkgs.overlays = [ self.overlays.default ];
             suckless =
               let
                 inherit (lib) mkDefault;
@@ -42,12 +29,13 @@
                 dmenu = mkDefault true;
                 st = mkDefault true;
               };
-          };
+          }
+        );
       };
 
-      overlays.default = final: _: suckless-packages { pkgs = final; };
+      overlays.default = final: _: suckless.packages { pkgs = final; };
 
-      packages = eachSystem (pkgs: suckless-packages { inherit pkgs; });
+      packages = eachSystem (pkgs: suckless.packages { inherit pkgs; });
 
       devShells = eachSystem (pkgs: {
         default = pkgs.mkShell {
