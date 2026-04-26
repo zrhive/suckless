9520345bcfc28584d63d632faf1c8b2114beca8a
zhyie <128297362+zhyie@users.noreply.github.com>
2026-04-26 14:05:56 +0800

    feat: devshells environment\

--- flake.nix
+++ flake.nix
@@ -1,10 +1,34 @@
 {
   description = "suckless configuration that sucks less";
 
-  outputs = { self }: {
-    nixosModules = {
-      suckless = import ./.;
-      default = self.nixosModules.suckless;
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" ];
+      eachSystem = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      nixosModules = {
+        suckless = import ./.;
+        default = self.nixosModules.suckless;
+      };
+
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = builtins.attrValues {
+            inherit (pkgs)
+              nixfmt
+
+              gcc
+              pkg-config
+              libx11
+              ;
+          };
+        };
+      });
+    };
 }
