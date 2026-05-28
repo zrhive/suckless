dda9fa11834a438a5dfc98f338d76a090fad788e
zhyie <128297362+zhyie@users.noreply.github.com>
2026-05-28 18:54:51 +0800

    Run flexipatch finalizer for st flexipatch

---
+++ flexipatch/st/patch/x_include.c
@@ -0,0 +1,6 @@
+/* Patches */
+#include "alpha.c"
+#include "boxdraw.c"
+#include "drag-n-drop.c"
+#include "font2.c"
+#include "openurlonclick.c"
---
+++ flexipatch/st/patch/x_include.h
@@ -0,0 +1,5 @@
+/* Patches */
+#include "alpha.h"
+#include "boxdraw.h"
+#include "drag-n-drop.h"
+#include "font2.h"
