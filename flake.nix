5a28a891f5fedb8589c7672632d99de3a3df6307
zhyie <128297362+zhyie@users.noreply.github.com>
2026-05-29 15:58:50 +0800

    feat: git hooks with prek

--- flake.nix
+++ flake.nix
@@ -6,39 +6,51 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: let
-    inherit (nixpkgs) lib;
-    systems = lib.systems.flakeExposed;
-    eachSystem = f: lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
-  in
-  {
-    nixosModules = {
-      default = ./nix/nixos.nix;
-      suckless = self.nixosModules.default;
-      flexipatch = self.nixosModules.default // ./nix/common.nix { inherit self; };
+  outputs =
+    { self, nixpkgs }:
+    let
+      inherit (nixpkgs) lib;
+      systems = lib.systems.flakeExposed;
+      eachSystem = f: lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      nixosModules = {
+        default = ./nix/nixos.nix;
+        suckless = self.nixosModules.default;
+        flexipatch = self.nixosModules.default // ./nix/common.nix { inherit self; };
+      };
+
+      homeModules = {
+        default = ./nix/home.nix;
+        suckless = self.homeModules.default;
+        flexipatch = self.homeModules.default // ./nix/common.nix { inherit self; };
+      };
+
+      packages = eachSystem (
+        pkgs:
+        import ./nix/packages.nix { inherit self pkgs; }
+        // {
+          test = {
+            dmenu = (pkgs.dmenu.overrideAttrs { src = "${self}/flexipatch/dmenu"; }).override { };
+          };
+        }
+      );
+
+      overlays.default = final: _prev: {
+        # inherit (import ./nix/packages.nix { pkgs = final; }) suckless flexipatch;
+        inherit (self.packages.${final.stdenv.hostPlatform.system}) suckless flexipatch;
+      };
+
+      # `$ prek install --prepare-hooks`
+      devShell = eachSystem (
+        pkgs:
+        pkgs.mkShellNoCC {
+          packages = [
+            (pkgs.callPackage ./treefmt.nix { inherit self; })
+            pkgs.prek
+            pkgs.jujutsu
+          ];
+        }
+      );
     };
-
-    homeModules = {
-      default = ./nix/home.nix;
-      suckless = self.homeModules.default;
-      flexipatch = self.homeModules.default // ./nix/common.nix { inherit self; };
-    };
-
-    packages = eachSystem (pkgs: import ./nix/packages.nix { inherit self pkgs; }
-      // {
-        test = {
-          dmenu = (pkgs.dmenu.overrideAttrs { src = "${self}/flexipatch/dmenu"; }).override {};
-        };
-      }
-    );
-
-    overlays.default = final: _prev: {
-      # inherit (import ./nix/packages.nix { pkgs = final; }) suckless flexipatch;
-      inherit (self.packages.${final.stdenv.hostPlatform.system}) suckless flexipatch;
-    };
-
-    devShell = eachSystem (pkgs: pkgs.mkShell {
-      packages = [ pkgs.nixfmt pkgs.statix pkgs.deadnix ];
-    });
-  };
 }
---
+++ treefmt.nix
@@ -0,0 +1,59 @@
+{
+  self,
+  formats,
+  writeShellScriptBin,
+
+  treefmt,
+  nixfmt,
+  deadnix,
+  statix,
+}:
+let
+  statixFix = writeShellScriptBin "statix-fix" ''
+    for file in "$@"; do
+      ${statix}/bin/statix fix --config ${
+        (formats.toml { }).generate "statix.toml" {
+          disbaled = [
+            "manual_inherit_from"
+            "empty_pattern"
+            "redundant_pattern_bind"
+            "repeated_keys"
+          ];
+        }
+      } "$file"
+    done
+  '';
+in
+treefmt.withConfig {
+  runtimeInputs = [
+    nixfmt
+    deadnix
+    statixFix
+  ];
+
+  settings = {
+    tree-root-file = "${self}/flake.nix";
+    on-unmatched = "info";
+
+    formatter = {
+      nixfmt = {
+        command = "nixfmt";
+        includes = [ "*.nix" ];
+      };
+
+      deadnix = {
+        command = "deadnix";
+        options = [
+          "--edit"
+          "--no-lambda-arg"
+        ];
+        includes = [ "*.nix" ];
+      };
+
+      statix = {
+        command = "statix-fix";
+        includes = [ "*.nix" ];
+      };
+    };
+  };
+}
