eb253dc683e626ab88df08b8a503f8b9bfa34699
zhyie <39026248-zhyie@users.noreply.gitlab.com>
2026-06-12 22:14:27 +0800

    refactor: flatten xserver configuration Remove redundant services.xserver config.

--- nix/modules/nixos.nix
+++ nix/modules/nixos.nix
@@ -9,17 +9,9 @@ in
 
   environment.systemPackages = packages;
 
-  services.xserver = {
-    enable = mkDefault true;
-
-    windowManager.dwm = {
-      enable = mkDefault tools.dwm.enable;
-      package = mkDefault tools.dwm.package;
-      extraSessionCommands = mkDefault extraCommands;
-    };
-
-    # Set to lightdm false by default, avoid
-    # conflict with other display manager config
-    displayManager.lightdm.enable = lib.mkDefault false;
+  services.xserver.windowManager.dwm = {
+    enable = mkDefault tools.dwm.enable;
+    package = mkDefault tools.dwm.package;
+    extraSessionCommands = mkDefault extraCommands;
   };
 }
